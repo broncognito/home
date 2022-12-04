2947aa119de09622157e71b73d92189b4783e627
broncognito <109101838+broncognito@users.noreply.github.com>
2022-12-04 16:33:21 -0500

    Add files via upload
    these files all work for a fully-functioning version of the website for all 4 courses!

--- login-page.js
+++ login-page.js
@@ -8,9 +8,19 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "mourya" && password === "123") {
+    if (username === "mourya" && password === "m53r84") {
        setTimeout(function(){window.location.href='mouryaCourseProf.html'}, 1000); 
     } 
+    else if (username === "ali" && password === "m64a28") {
+       setTimeout(function(){window.location.href='aliCourseProf.html'}, 1000); 
+    } 
+    else if (username === "lu" && password === "d38u09") {
+       setTimeout(function(){window.location.href='luCourseProf.html'}, 1000); 
+    } 
+    else if (username === "luo" && password === "j40z12") {
+       setTimeout(function(){window.location.href='luoCourseProf.html'}, 1000); 
+    } 
+    
     else {
         loginErrorMsg.style.opacity = 1;
     }
@@ -26,9 +36,19 @@ loginButton2.addEventListener("click", (e) => {
     e.preventDefault();
     const password = loginForm2.password.value;
 
-    if (password === "mourya123") {
+    if (password === "mourya8294") {
        setTimeout(function(){window.location.href='mouryaCourse.html'}, 1000); 
-    } 
+    }
+    else if (password === "ali2097") {
+       setTimeout(function(){window.location.href='aliCourse.html'}, 1000); 
+    }
+    else if (password === "lu1754") {
+       setTimeout(function(){window.location.href='luCourse.html'}, 1000); 
+    }
+    else if (password === "luo3259") {
+       setTimeout(function(){window.location.href='luoCourse.html'}, 1000); 
+    }
+    
     else {
         loginErrorMsg2.style.opacity = 1;
     }
